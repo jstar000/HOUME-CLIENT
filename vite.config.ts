117e82373bd985834a317e6bd19b693dc074b1a8
Soy Park <90364636+soyyyyy@users.noreply.github.com>
2025-07-01 18:13:50 +0900

    [chore] ESLint&Prettier 세팅, husky 세팅, path alias 설정 (#15)
    * chore: eslint+prettier 세팅
    
    * chore: husky 세팅
    
    * chore: path alias 설정
    
    * chore: gitignore 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@apis': path.resolve(__dirname, 'src/shared/apis'),
+      '@assets': path.resolve(__dirname, 'src/shared/assets'),
+      '@components': path.resolve(__dirname, 'src/shared/components'),
+      '@constants': path.resolve(__dirname, 'src/shared/constants'),
+      '@hooks': path.resolve(__dirname, 'src/shared/hooks'),
+      '@styles': path.resolve(__dirname, 'src/shared/styles'),
+      '@types': path.resolve(__dirname, 'src/shared/types'),
+      '@utils': path.resolve(__dirname, 'src/shared/utils'),
+    },
+  },
+});
