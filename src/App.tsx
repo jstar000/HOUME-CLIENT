4f0d3d9e97876f15ad7e34510d36324491aa900f
Kyoungho Eom <kheom0512@gmail.com>
2025-07-04 21:33:55 +0900

    feat: 기본 앱 컴포넌트 단순화

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-import { useState } from 'react';
-
-import ViteLogo from '/vite.svg?react';
+import ViteLogo from '../public/vite.svg?react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <ViteLogo className="logo" title="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <ViteLogo className="logo" title="Vite logo" />;
 }
 
 export default App;
