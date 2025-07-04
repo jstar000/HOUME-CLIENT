d5bcb90c30532611f11a707c17471bbcc7207944
Kyoungho Eom <kheom0512@gmail.com>
2025-07-04 21:32:02 +0900

    style: Vite 로고 SVG 컴포넌트로 교체

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+import ViteLogo from '/vite.svg?react';
 import './App.css';
 
 function App() {
@@ -10,10 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <ViteLogo className="logo" title="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
