3674ea348ed5430e7e4a59fd592ccaae3707092a
Kyoungho Eom <kheom0512@gmail.com>
2025-07-04 21:26:30 +0900

    chore: SVG React 컴포넌트 변환 및 svgr 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          floatPrecision: 2,
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
