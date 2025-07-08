1ab28426e1c6328ec5200b7481075a073c771274
jstar_00 <wltjd6300@naver.com>
2025-07-08 18:36:36 +0900

    fix: storybook 코드 수정
    - CSF v3에서 제공하는 StoryObj 객체형 스토리 정의 방식에 알맞게 코드 수정

--- src/shared/components/overlay/Modal.stories.tsx
+++ src/shared/components/overlay/Modal.stories.tsx
@@ -1,47 +1,52 @@
-import Modal, { type ModalProps } from './Modal';
-import type { Meta, StoryFn } from '@storybook/react-vite';
+import Modal from './Modal';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
-const meta: Meta<typeof Modal> = {
+const meta = {
   title: 'shared/components/overlay/Modal',
   component: Modal,
   argTypes: {
     isOpen: { control: 'boolean' },
     title: { control: 'text' },
-    close: { action: 'closed' },
+    onClose: { action: 'onClose' },
   },
   parameters: {
     layout: 'padded',
     docs: {
       description: {
         component:
-          'toss/overlay-kit을 사용해 구현한 모달창입니다. \n' +
+          'toss/overlay-kit을 사용해 구현한 모달창입니다.\n' +
           '나가기 버튼을 탭할 시 모달창이 unmount됩니다.',
       },
     },
   },
-} as Meta<typeof Modal>;
+} satisfies Meta<typeof Modal>;
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-const Template: StoryFn<ModalProps> = (args) => <Modal {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  isOpen: true,
-  title: '스타일링 이미지대로 가구를 추천 받으려면 크레딧이 필요해요',
+export const Default: Story = {
+  args: {
+    isOpen: true,
+    title: '스타일링 이미지대로 가구를 추천 받으려면 크레딧이 필요해요',
+    onClose: () => {},
+  },
 };
 
-export const Closed = Template.bind({});
-Closed.args = {
-  isOpen: false,
-  title: '모달이 닫혀 있는 경우',
+export const Closed: Story = {
+  args: {
+    isOpen: false,
+    title: '모달이 닫혀 있는 경우',
+    onClose: () => {},
+  },
 };
 
-export const Interactive = Template.bind({});
-Interactive.args = {
-  isOpen: true,
-  title: 'Interactive Modal',
-};
-Interactive.parameters = {
-  actions: { handles: ['click button'] },
+export const Interactive: Story = {
+  args: {
+    isOpen: true,
+    title: 'Interactive Modal',
+    onClose: () => {},
+  },
+  parameters: {
+    actions: { handles: ['click button'] },
+  },
 };
