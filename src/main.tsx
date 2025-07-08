a41e0855d8bc50e6d43f212b16de9cac9354d481
문혜성 <coldute1014@naver.com>
2025-07-08 11:58:16 +0900

    chore: 디자인 시스템 수정사항 반영

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './shared/apis/queryClient.ts';
 import App from './App.tsx';
+import '@/shared/styles/global.css.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/shared/styles/global.css.ts
+++ src/shared/styles/global.css.ts
@@ -1,6 +1,7 @@
 import { globalStyle } from '@vanilla-extract/css';
 import { colorVars } from '@styles/tokens/color.css';
 import { fontVars } from '@styles/tokens/font.css';
+import '@styles/reset.css.ts';
 import '@/shared/styles/fontFace.css';
 
 globalStyle('html, body', {
