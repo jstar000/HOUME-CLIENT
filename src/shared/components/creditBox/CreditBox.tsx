e0f16b550b2e52aed6016a36c4d224ad087b2128
Soy <da5868@naver.com>
2025-07-09 16:50:47 +0900

    chore: 코드래빗 반영

--- src/shared/components/creditBox/CreditBox.tsx
+++ src/shared/components/creditBox/CreditBox.tsx
@@ -8,10 +8,14 @@ const CreditBox = ({ credit }: CreditBoxProps) => {
   return (
     <div className={styles.boxWrapper}>
       <div className={styles.textContainer}>
-        <span className={styles.infoText}>보유 크레딧</span>
-        <span className={styles.creditText}>{credit}</span>
+        <span className={styles.infoText} id="credit-label">
+          보유 크레딧
+        </span>
+        <span className={styles.creditText} aria-labelledby="credit-label">
+          {credit}
+        </span>
       </div>
-      <ChargeButton children="충전하기" />
+      <ChargeButton>충전하기</ChargeButton>
     </div>
   );
 };
