04c2970995f594a424ef273c496b79d2c6e6abed
Soy <da5868@naver.com>
2025-07-09 18:28:26 +0900

    rename: 파일명 수정

---
+++ src/shared/components/button/showErrorButton/ShowErrorButton.tsx
@@ -0,0 +1,17 @@
+import WarningIcon from '@assets/icons/icn_warning_red.svg?react';
+import * as styles from './ShowErrorButton.css';
+
+interface ShowErrorMessageProps {
+  message: string;
+}
+
+const ShowErrorMessage = ({ message }: ShowErrorMessageProps) => {
+  return (
+    <div className={styles.messageWrapper}>
+      <WarningIcon />
+      <span className={styles.messageText}>{message}</span>
+    </div>
+  );
+};
+
+export default ShowErrorMessage;
