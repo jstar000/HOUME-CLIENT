d1e96ad049bbbe83cc337c1cb9297b32d44a5b1d
문혜성 <coldute1014@naver.com>
2025-07-10 04:35:05 +0900

    feat: divider 컴포넌트 구현

--- src/shared/components/divider/Divider.css.ts
+++ src/shared/components/divider/Divider.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { colorVars } from '@/shared/styles/tokens/color.css';
 
 export const divider = style({
-  width: '37.5rem',
+  width: '100%',
   height: '0.8rem',
   backgroundColor: colorVars.color.gray100,
 });
--- src/shared/components/divider/Divider.tsx
+++ src/shared/components/divider/Divider.tsx
@@ -1,7 +1,7 @@
 import * as styles from './Divider.css';
 
 const Divider = () => {
-  return <div className={styles.divider} />;
+  return <hr className={styles.divider} />;
 };
 
 export default Divider;
