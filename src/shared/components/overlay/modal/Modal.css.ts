d055bd7bfc41c7d497a804a97bbddf3f9b598484
jstar_00 <wltjd6300@naver.com>
2025-07-11 00:58:28 +0900

    chore

--- src/shared/components/overlay/modal/Modal.css.ts
+++ src/shared/components/overlay/modal/Modal.css.ts
@@ -15,7 +15,7 @@ export const container = style({
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  zIndex: 999,
+  zIndex: zIndex.modal,
   display: 'flex',
   flexDirection: 'column',
   width: '30rem',
