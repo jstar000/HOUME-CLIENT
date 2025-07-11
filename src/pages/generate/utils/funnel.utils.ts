c3f080b1259dc8c4262ed7d4b84c25fbf8f756df
jstar_00 <wltjd6300@naver.com>
2025-07-11 16:25:13 +0900

    refactor: 불필요한 함수 제거

--- src/pages/generate/utils/funnel.utils.ts
+++
@@ -1,7 +0,0 @@
-export const createStepHandler = <T>(
-  dispatch: (eventName: string, data: T) => void,
-  eventName: string
-) => {
-  // create prefix: React에서 '함수를 만들어주는 함수'임을 나타내는 prefix 컨벤션
-  return (data: T) => dispatch(eventName, data);
-};
