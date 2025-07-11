29ed81d82e510b60d96606913aaebbe25da5a769
earl9rey <earl9rey@naver.com>
2025-07-11 14:45:37 +0900

    feat: 소셜 로그인 진입 페이지 뷰 구현

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,37 @@
+import * as styles from './loginPage.css';
+import CtaButton from '@/shared/components/button/ctaButton/CtaButton';
+import TitleNavBar from '@/shared/components/navBar/TitleNavBar';
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className={styles.container}>
+      <TitleNavBar
+        title="회원가입/로그인"
+        isBackIcon={false}
+        isLoginBtn={false}
+      />
+      <div className={styles.textbox}>
+        <h1 className={styles.title}>하우미와 나다운 집을 만들어가요!</h1>
+        <p className={styles.content}>
+          로그인하면 매번 집, 취향, 활동 정보를 입력할 필요없이 <br /> 스타일링
+          결과를 저장하고 다시 볼 수 있어요.
+        </p>
+      </div>
+      <div className={styles.imgbox}></div>
+      <div className={styles.btnarea}>
+        <CtaButton typeVariant="kakao">카카오 로그인</CtaButton>
+        <aside className={styles.aside}>
+          가입 시{' '}
+          <a href="https://github.com/TEAM-HOUME" className={styles.link}>
+            서비스 약관
+          </a>{' '}
+          및{' '}
+          <a href="https://github.com/TEAM-HOUME" className={styles.link}>
+            개인정보처리방침
+          </a>
+          에 동의한 것으로 간주합니다.
+        </aside>
+      </div>
     </div>
   );
 };
