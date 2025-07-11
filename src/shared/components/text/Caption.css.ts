1d026031d40e92540185bc7e1bb7c8cff0b58402
문혜성 <coldute1014@naver.com>
2025-07-11 23:46:06 +0900

    refactor: 코드 리뷰 반영

--- src/shared/components/text/Caption.css.ts
+++ src/shared/components/text/Caption.css.ts
@@ -1,4 +1,4 @@
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 import { colorVars } from '@/shared/styles/tokens/color.css';
 import { fontStyle } from '@/shared/styles/fontStyle';
 
@@ -7,27 +7,27 @@ export const captionBox = style({
   alignItems: 'center',
   gap: '0.4rem',
   padding: '0.8rem 1.2rem',
-  borderRadius: '0.6rem',
+  borderRadius: '6px',
   backgroundColor: colorVars.color.gray000,
-  border: `1px solid ${colorVars.color.gray200}`,
+  border: `1px solid ${colorVars.color.gray300}`,
   boxSizing: 'border-box',
-  maxWidth: '33.5rem',
+  width: '33.5rem',
 });
 
-export const strongText = style({
-  ...fontStyle('caption_sb_12'),
-  color: colorVars.color.gray900,
-});
-
-export const normalText = style({
-  ...fontStyle('caption_r_12'),
-  color: colorVars.color.gray500,
-});
-
-export const highlight = style({
-  ...fontStyle('caption_m_12'),
-  color: colorVars.color.gray000,
-  backgroundColor: colorVars.color.gray800,
-  padding: '0 0.4rem',
-  borderRadius: '0.3rem',
+export const textVariants = styleVariants({
+  strong: {
+    ...fontStyle('caption_sb_12'),
+    color: colorVars.color.gray900,
+  },
+  normal: {
+    ...fontStyle('caption_r_12'),
+    color: colorVars.color.gray500,
+  },
+  highlight: {
+    ...fontStyle('caption_m_12'),
+    color: colorVars.color.gray000,
+    backgroundColor: colorVars.color.gray800,
+    padding: '0 0.4rem',
+    borderRadius: '3px',
+  },
 });
--- src/shared/components/text/Caption.tsx
+++ src/shared/components/text/Caption.tsx
@@ -1,12 +1,14 @@
-import { captionBox, highlight, strongText, normalText } from './Caption.css';
+import { captionBox, textVariants } from './Caption.css';
 
-export const Caption = () => {
+const Caption = () => {
   return (
     <div className={captionBox}>
-      <span className={strongText}>휴식형</span>
-      <span className={normalText}>을 선택하면</span>
-      <span className={highlight}>소파</span>
-      <span className={normalText}>가 이미지에 포함돼요.</span>
+      <span className={textVariants.strong}>휴식형</span>
+      <span className={textVariants.normal}>을 선택하면</span>
+      <span className={textVariants.highlight}>소파</span>
+      <span className={textVariants.normal}>가 이미지에 포함돼요.</span>
     </div>
   );
 };
+
+export default Caption;
