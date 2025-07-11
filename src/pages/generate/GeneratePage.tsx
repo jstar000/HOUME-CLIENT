b25ee4d6f71423c9890247b060a817acab09c6cc
Soy <da5868@naver.com>
2025-07-11 22:02:15 +0900

    feat: 구현 중

--- src/pages/generate/GeneratePage.tsx
+++ src/pages/generate/GeneratePage.tsx
@@ -9,26 +9,7 @@ const GeneratePage = () => {
         isBackIcon={true}
         isLoginBtn={false}
       />
-      <section>
-        {' '}
-        전체 컨테이너
-        <div>
-          {' '}
-          윗 부분 영역
-          <div>프로그레스바 영억</div>
-          <p>
-            마음에 드는 가구를 선택하면, <br />
-            하우미가 사용자님의 취향을 더 잘 이해할 수 있어요!
-          </p>
-        </div>
-        <section>
-          캐러셀 영역
-          <div>
-            <LoadImageArea />
-          </div>
-          <div>버튼그룹 </div>
-        </section>
-      </section>
+      <LoadImageArea />
     </main>
   );
 };
--- src/pages/generate/loading/LoadingPage.tsx
+++ src/pages/generate/loading/LoadingPage.tsx
@@ -1,14 +1,8 @@
 import LoadImageArea from '@assets/loadImgArea.svg?react';
-import TitleNavBar from '@/shared/components/navBar/TitleNavBar';
 
 const LoadingPage = () => {
   return (
     <main>
-      <TitleNavBar
-        title={'스타일링 이미지 생성'}
-        isBackIcon={true}
-        isLoginBtn={false}
-      />
       <section>
         {' '}
         전체 컨테이너
