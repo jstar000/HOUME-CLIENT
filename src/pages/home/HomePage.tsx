103b007e0a7e58ff19b43841fff8ad5539d81c92
earl9rey <earl9rey@naver.com>
2025-07-11 23:16:00 +0900

    chore: 쿠키 테스트용 버튼 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import LogoutButton from '../login/components/LogoutButton';
 import TokenRefreshTest from '../login/components/TokenRefreshTest';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Home Page</h1>
       <TokenRefreshTest />
       <LogoutButton />
+      <br />
+      <button onClick={() => navigate('/signup')}>Signup</button>
+      <br />
+      <br />
+      <button onClick={() => navigate(0)}>새로고침</button>
     </div>
   );
 };
