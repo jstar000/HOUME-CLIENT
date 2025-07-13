10715b9ede8b26f7753b8f744a059dd074b1d9e0
earl9rey <earl9rey@naver.com>
2025-07-13 23:34:03 +0900

    fix: OAuth 페이지 리다이렉트 외부 연결 해결

--- src/pages/login/components/KakaoButton.tsx
+++ src/pages/login/components/KakaoButton.tsx
@@ -1,12 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import { KAKAO_AUTH_URL } from '../constants/kakaoLoginPath';
 import CtaButton from '@/shared/components/button/ctaButton/CtaButton';
 
 const KakaoButton = () => {
-  const navigate = useNavigate();
-
   const handleLogin = () => {
-    navigate(KAKAO_AUTH_URL);
+    // 외부 URL로 리다이렉트하기 위해 window.location.href 사용
+    window.location.href = KAKAO_AUTH_URL;
   };
 
   return (
