c3e2e6a2cda41596bf9a545e2f7d2cae996c5850
Soy <da5868@naver.com>
2025-07-14 23:50:25 +0900

    fix: 빌드 에러 해결

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,20 @@
 import { useNavigate } from 'react-router-dom';
 import { LogoutButton } from '../login/components/LogoutButton';
 import TokenRefreshTest from '../login/components/TokenRefreshTest';
-import FloorPlan from '../onboarding/components/steps/step2/FloorPlan';
 
 const HomePage = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <FloorPlan />
+      <h1>Home Page</h1>
+      <TokenRefreshTest />
+      <LogoutButton />
+      <br />
+      <button onClick={() => navigate('/signup')}>Signup</button>
+      <br />
+      <br />
+      <button onClick={() => navigate(0)}>새로고침</button>
     </div>
   );
 };
