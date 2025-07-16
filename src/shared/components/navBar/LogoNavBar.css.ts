48a374d66a71b617472fa9dbe39a517f772159e3
earl9rey <earl9rey@naver.com>
2025-07-17 02:12:25 +0900

    design: LogoNavBar 디자인 변경사항 적용

--- src/shared/components/navBar/LogoNavBar.css.ts
+++ src/shared/components/navBar/LogoNavBar.css.ts
@@ -6,7 +6,6 @@ export const container = style({
   display: 'flex',
   width: '100%',
   height: '7.2rem',
-  paddingRight: '1.6rem',
   justifyContent: 'space-between',
   alignItems: 'center',
   textAlign: 'center',
@@ -31,7 +30,7 @@ export const rightdiv = style({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  width: '4.8rem',
-  height: '7.2rem',
-  padding: '1.2rem 0',
+  width: '8rem',
+  height: '4.8rem',
+  padding: '1.2rem 1.6rem',
 });
