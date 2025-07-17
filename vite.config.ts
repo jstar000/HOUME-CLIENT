d20dc3d24a45e3d8c1be2acf6202c39980b93f8a
jstar000 <wltjd6300@gmail.com>
2025-07-17 21:50:26 +0000

    chore: 빌드 설정 조건 추가

--- vite.config.ts
+++ vite.config.ts
@@ -33,9 +33,13 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({
-            configDir: path.join(dirname, '.storybook'),
-          }),
+          ...(process.env.NODE_ENV !== 'production'
+            ? [
+                storybookTest({
+                  configDir: path.join(dirname, '.storybook'),
+                }),
+              ]
+            : []),
         ],
         test: {
           name: 'storybook',
