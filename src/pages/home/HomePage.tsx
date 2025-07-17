f7041e1d2b3d9e418e6018148f70e16dff3c5f8b
Soy <da5868@naver.com>
2025-07-17 19:38:24 +0900

    chore: 홈 페이지 토큰 테스트 컴포넌트 제거

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,6 @@ import IntroSection from './components/introSection/IntroSection';
 import StepGuideSection from './components/stepGuideSection/StepGuideSection';
 import ReviewSection from './components/reviewSection/ReviewSection';
 import * as styles from './HomePage.css';
-import TokenRefreshTest from '../login/components/TokenRefreshTest';
 import CtaButton from '@/shared/components/button/ctaButton/CtaButton';
 import { useUserStore } from '@/store/useUserStore';
 
@@ -21,7 +20,6 @@ const HomePage = () => {
         <LogoNavBar buttonType={isLoggedIn ? 'profile' : 'login'} />
         <div className={styles.introSection}>
           <IntroSection />
-          <TokenRefreshTest />
         </div>
       </div>
       <div className={styles.contents}>
