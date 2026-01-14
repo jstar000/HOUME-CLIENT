e5a776e66c557014cfbeb6902741445055405211
Kyoungho Eom <kheom0512@gmail.com>
2026-01-15 00:53:53 +0900

    fix: 소셜 회원가입 API POST로 변경

--- src/pages/signup/hooks/usePatchSignup.ts
+++ src/pages/signup/hooks/usePatchSignup.ts
@@ -6,22 +6,24 @@ import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/routes/paths';
 import { useUserStore } from '@/store/useUserStore';
 
-import { patchSignup } from '../apis/signup';
+import { postSignup } from '../apis/signup';
 
 import type { SignupRequest, SignupResponse } from '../types/apis/signup';
 
 export const usePatchSignup = () => {
   const navigate = useNavigate();
   const setUserName = useUserStore((state) => state.setUserName);
+  const setAccessToken = useUserStore((state) => state.setAccessToken);
 
   return useMutation<SignupResponse, Error, SignupRequest>({
-    mutationFn: patchSignup,
+    mutationFn: postSignup,
     retry: 3,
     // 회원가입 성공 시 실행되는 함수
     onSuccess: (response) => {
       // console.log('[usePatchSignup] 회원가입 성공:', response);
-      // console.log(response.data);
-      setUserName(response.data); // userName 전역 저장
+      setUserName(response.userName); // userName 전역 저장
+      setAccessToken(response.accessToken);
+      sessionStorage.removeItem('signupToken');
       // 회원가입 완료 페이지 이동
       navigate(ROUTES.GENERATE_START);
     },
