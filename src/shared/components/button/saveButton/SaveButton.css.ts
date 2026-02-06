e619efaed41d32f977e2c79c207f5f7e94dc667e
Kyoungho Eom <kheom0512@gmail.com>
2026-02-06 17:22:47 +0900

    refactor: 하트 색상 제어를 코드로 전환

--- src/shared/components/button/saveButton/SaveButton.css.ts
+++ src/shared/components/button/saveButton/SaveButton.css.ts
@@ -1,4 +1,6 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
+
+import { colorVars } from '@/shared/styles/tokens/color.css';
 
 export const buttonWrapper = style({
   width: '2.8rem',
@@ -10,4 +12,24 @@ export const buttonWrapper = style({
   border: 'none',
   padding: 0,
   margin: 0,
+});
+
+export const selected = style({});
+
+export const unselected = style({});
+
+globalStyle(`${buttonWrapper} svg`, {
+  width: '2.4rem',
+  height: '2.4rem',
+});
+
+globalStyle(`${selected} svg path`, {
+  fill: colorVars.color.primary,
+  stroke: 'none',
+});
+
+globalStyle(`${unselected} svg path`, {
+  fill: 'rgba(0, 0, 0, 0.2)',
+  stroke: colorVars.color.gray000,
+  strokeWidth: 1,
 });
