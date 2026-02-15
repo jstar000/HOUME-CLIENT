40461ffb2e64a9a19a0813363d86c03239c18618
jstar_00 <wltjd6300@naver.com>
2026-02-16 01:49:39 +0900

    fix: InlineError 부모 페이지 flex: 1 적용

--- src/pages/mypage/MyPage.css.ts
+++ src/pages/mypage/MyPage.css.ts
@@ -1,6 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
 export const contentWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
   minHeight: '100vh',
   alignItems: 'center',
   alignSelf: 'stretch',
