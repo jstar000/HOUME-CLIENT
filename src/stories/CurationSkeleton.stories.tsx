97e5abd17d3a3c0aec9bd89ce48aa911ba10f9d2
earl9rey <earl9rey@naver.com>
2026-03-01 01:26:02 +0900

    refactor: import문 절대경로 변경

--- src/stories/CurationSkeleton.stories.tsx
+++ src/stories/CurationSkeleton.stories.tsx
@@ -1,8 +1,8 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import TitleNavBar from '@components/navBar/TitleNavBar';
+import CurationSkeleton from '@pages/generate/pages/result/curationSection/CurationSkeleton';
 
-import CurationSkeleton from '@/pages/generate/pages/result/curationSection/CurationSkeleton';
+import TitleNavBar from '@components/navBar/TitleNavBar';
 
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
