91bdfdc287cb67e4ec52578f05a9d5fc26a1a041
jstar_00 <wltjd6300@naver.com>
2026-03-06 23:10:58 +0900

    style: WelcomePage 디자인 수정 반영

--- src/pages/generate/pages/welcome/WelcomePage.tsx
+++ src/pages/generate/pages/welcome/WelcomePage.tsx
@@ -11,10 +11,9 @@ import { useUserStore } from '@store/useUserStore';
 
 import { TOAST_TYPE } from '@shared/types/toast';
 
-import SignupImage from '@assets/icons/loginAfter.png';
+import SignupImage from '@assets/images/signupWelcome.png';
 
 import CtaButton from '@components/button/ctaButton/CtaButton';
-import TitleNavBar from '@components/navBar/TitleNavBar';
 import { useToast } from '@components/toast/useToast';
 
 import { getLoginRedirect, consumeLoginRedirect } from '@utils/loginRedirect';
@@ -52,24 +51,24 @@ const WelcomePage = () => {
 
   return (
     <div className={styles.container}>
-      <TitleNavBar title="시작하기" isBackIcon={false} isLoginBtn={false} />
-      <div className={styles.textbox}>
-        <h1 className={styles.title}>
-          {userName ? `${userName}님, 지금 바로` : '하우미님, 지금 바로'}
-          <br />집 꾸미기 시작해봐요!
-        </h1>
-        <p className={styles.content}>
-          AI 이미지처럼 내 집을 꾸밀 수 있도록 <br />
-          구매 가능한 상품까지 바로 추천 해드려요.
-        </p>
-      </div>
-      <div className={styles.imgbox}>
-        <img
-          src={SignupImage}
-          alt="회원가입 완료 이미지"
-          className={styles.signUpImg}
-          loading="lazy"
-        />
+      <div className={styles.contents}>
+        <div className={styles.imgbox}>
+          <img
+            src={SignupImage}
+            alt="회원가입 완료 이미지"
+            className={styles.signUpImg}
+          />
+        </div>
+        <div className={styles.textbox}>
+          <h1 className={styles.title}>
+            {userName ? `${userName}님, 지금 바로` : '하우미님, 지금 바로'}
+            <br />집 꾸미기 시작해봐요!
+          </h1>
+          <p className={styles.content}>
+            24시간 동안 무료로 이미지를 생성할 수 있는 <br />
+            토큰 N개를 선물로 드렸어요.
+          </p>
+        </div>
       </div>
       <div className={styles.btnarea}>
         <CtaButton onClick={handleCtaClick}>
