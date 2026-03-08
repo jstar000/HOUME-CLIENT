1cbebfc7f857f35cceba69a7474fa943a221fe5c
earl9rey <earl9rey@naver.com>
2026-03-08 22:16:58 +0900

    chore: fontStyle helper 선언 위치 변경

--- src/shared/styles/fontStyle.ts
+++ src/shared/styles/fontStyle.ts
@@ -1,4 +1,5 @@
 import { fontVars } from '@styles/tokens/font.css';
+import { fontVars as fontVarsV2 } from '@styles/tokensV2/font.css';
 
 type FontKeys = Exclude<keyof typeof fontVars, 'family'>;
 
@@ -8,6 +9,23 @@ export const fontStyle = (key: FontKeys) => {
 
   return {
     fontFamily: fontVars.family.pretendard,
+    fontSize: style.size,
+    fontWeight: style.weight,
+    lineHeight: style.lineHeight,
+    letterSpacing: style.letterSpacing,
+  };
+};
+
+const v2 = fontVarsV2.font;
+type FontV2Keys = Exclude<keyof typeof v2, 'family'>;
+
+export const fontStyleV2 = (key: FontV2Keys) => {
+  const style = v2[key];
+  if (!style || !('size' in style))
+    throw new Error(`Invalid font v2 key: ${key}`);
+
+  return {
+    fontFamily: v2.family.pretendard,
     fontSize: style.size,
     fontWeight: style.weight,
     lineHeight: style.lineHeight,
