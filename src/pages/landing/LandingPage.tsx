b59db45eb3a1316552a52c381f5c3f717c836a54
earl9rey <earl9rey@naver.com>
2026-03-12 02:59:56 +0900

    feat: 랜딩페이지 뷰 1차 구현

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,16 +1,26 @@
 import CtaButtonV2 from '@/shared/components/button/ctaButton/CtaButtonV2';
 
+import NavBar from './components/NavBar';
 import * as styles from './LandingPage.css';
 
 const LandingPage = () => {
   return (
     <main className={styles.page}>
-      <header className={styles.headerSection}>{/* 네비바 영역 */}</header>
+      <NavBar />
       <section className={styles.mainSection}>
         <div className={styles.contentBlock}>
-          <p className={styles.text}>텍스트 영역</p>
+          <div className={styles.textContainer}>
+            <h2 className={styles.title}>
+              '재택근무가 필요한'
+              <br />
+              나를 위한 맞춤형 인테리어는?
+            </h2>
+            <p className={styles.text}>
+              집 구조, 취향, 생활 방식까지 반영하는 AI 홈 스타일링
+            </p>
+          </div>
           <CtaButtonV2
-            height={56}
+            height={44}
             buttonStyle="fill"
             color="inverse"
             icon="doublestar"
