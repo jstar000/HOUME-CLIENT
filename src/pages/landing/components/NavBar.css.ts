b5bd3fd6018e50c3709977218fa00da23237c8c1
earl9rey <earl9rey@naver.com>
2026-03-12 03:01:24 +0900

    style: 랜딩 전용 navBar css 적용

---
+++ src/pages/landing/components/NavBar.css.ts
@@ -0,0 +1,31 @@
+import { style } from '@vanilla-extract/css';
+
+import { unitVars } from '@styles/tokensV2/unit.css';
+
+export const container = style({
+  display: 'flex',
+  flexShrink: 0,
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: `${unitVars.unit.gapPadding['000']}`,
+  width: '100%',
+  height: '4.8rem',
+  minHeight: '5.6rem',
+});
+
+export const leftDiv = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: unitVars.unit.gapPadding['200'],
+  padding: `0 ${unitVars.unit.gapPadding['500']}`,
+});
+
+export const rightDiv = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: `0 ${unitVars.unit.gapPadding['200']}`,
+  width: '8rem',
+  minHeight: '4.8rem',
+});
