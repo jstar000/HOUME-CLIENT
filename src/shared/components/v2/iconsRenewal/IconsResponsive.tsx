e53a4cdffb5c5a273a6a957699940fe6a15cbef8
Soy <da5868@naver.com>
2026-03-17 01:24:56 +0900

    feat: Icon import 및 size별 공컴

---
+++ src/shared/components/v2/iconsRenewal/IconsResponsive.tsx
@@ -0,0 +1,61 @@
+import ArrowLeft from '@assets/v2/svg/ArrowLeft.svg?url';
+import ArrowLeftFill from '@assets/v2/svg/ArrowLeftFill.svg?url';
+import ArrowRight from '@assets/v2/svg/ArrowRight.svg?url';
+import ArrowRightFill from '@assets/v2/svg/ArrowRightFill.svg?url';
+import ArrowUp from '@assets/v2/svg/ArrowUp.svg?url';
+import ChevronDown from '@assets/v2/svg/ChevronDown.svg?url';
+import ChevronUp from '@assets/v2/svg/ChevronUp.svg?url';
+import Close from '@assets/v2/svg/Close.svg?url';
+import CloseFillBlack from '@assets/v2/svg/CloseFillBlack.svg?url';
+import CloseFillGray from '@assets/v2/svg/CloseFillGray.svg?url';
+import DoubleStar from '@assets/v2/svg/DoubleStar.svg?url';
+import FlipHorizontal from '@assets/v2/svg/FlipHorizontal.svg?url';
+import HeartFillColor from '@assets/v2/svg/HeartFillColor.svg?url';
+import HeartFillGray from '@assets/v2/svg/HeartFillGray.svg?url';
+import HeartStrokeGray from '@assets/v2/svg/HeartStrokeGray.svg?url';
+import HeartStrokeWhite from '@assets/v2/svg/HeartStrokeWhite.svg?url';
+import Link from '@assets/v2/svg/Link.svg?url';
+import PlusFill from '@assets/v2/svg/PlusFill.svg?url';
+import Profile from '@assets/v2/svg/Profile.svg?url';
+import Refresh from '@assets/v2/svg/Refresh.svg?url';
+import Search from '@assets/v2/svg/Search.svg?url';
+import ViewDetail from '@assets/v2/svg/ViewDetail.svg?url';
+
+const IconsName = {
+  ArrowLeft: ArrowLeft,
+  ArrowLeftFill: ArrowLeftFill,
+  ArrowRight: ArrowRight,
+  ArrowRightFill: ArrowRightFill,
+  ArrowUp: ArrowUp,
+  ChevronDown: ChevronDown,
+  ChevronUp: ChevronUp,
+  Close: Close,
+  CloseFillBlack: CloseFillBlack,
+  CloseFillGray: CloseFillGray,
+  DoubleStar: DoubleStar,
+  FlipHorizontal: FlipHorizontal,
+  HeartFillColor: HeartFillColor,
+  HeartFillGray: HeartFillGray,
+  HeartStrokeGray: HeartStrokeGray,
+  HeartStrokeWhite: HeartStrokeWhite,
+  Link: Link,
+  PlusFill: PlusFill,
+  Profile: Profile,
+  Refresh: Refresh,
+  Search: Search,
+  ViewDetail: ViewDetail,
+} as const;
+
+export type IconName = keyof typeof IconsName;
+export type IconSize = '40' | '32' | '24' | '20' | '16' | '14' | '12';
+
+export interface IconsResponsiveProps {
+  name: IconName;
+  size: IconSize;
+}
+
+const IconsResponsive = ({ name, size = '24' }: IconsResponsiveProps) => {
+  return <img src={IconsName[name]} alt={name} width={size} height={size} />;
+};
+
+export default IconsResponsive;
