b809fd2dd26e0e4fcd0d7916fa996e63ab90bc2f
Soy <da5868@naver.com>
2026-03-17 17:08:39 +0900

    test: test 파일 수정

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,8 +2,15 @@ import SearchBar from '@/shared/components/v2/textField/SearchBar';
 
 export const ComponentTest = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-      <SearchBar placeholder="기본 상태" />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '20px',
+        padding: '20px',
+      }}
+    >
+      <SearchBar />
     </div>
   );
 };
