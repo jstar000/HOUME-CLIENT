8615420436ac618f4dc5353048af02e0b37c72c0
Soy <da5868@naver.com>
2026-03-19 00:00:15 +0900

    chore: React.ComponentProps 사용하도록 변경

--- src/shared/components/v2/button/IconButton.tsx
+++ src/shared/components/v2/button/IconButton.tsx
@@ -1,39 +1,40 @@
-import IconsResponsive, { type IconName, type IconSize } from '../icon/Icon';
+import clsx from 'clsx';
+
+import { type IconName, type IconSize } from '../icon/Icon';
+import Icon from '../icon/Icon';
 import * as styles from '../icon/Icon.css';
 
 export type IconButtonSize = 'S' | 'M' | 'L' | 'XL';
 
-const BTN_ICON_SIZE: Record<IconButtonSize, IconSize> = {
+const ICON_BUTTON_SIZE: Record<IconButtonSize, IconSize> = {
   S: '20',
   M: '24',
   L: '32',
   XL: '40',
 } as const;
 
-export interface IconButtonProps {
+export interface IconButtonProps
+  extends Omit<React.ComponentProps<'button'>, 'children'> {
   name: IconName;
   size?: IconButtonSize;
-  disabled?: boolean;
-  onClick?: () => void;
 }
 
 const IconButton = ({
   name,
   size = 'S',
+  type = 'button',
+  className,
   disabled = false,
-  onClick,
+  ...props
 }: IconButtonProps) => {
   return (
     <button
-      type="button"
-      className={styles.iconButton({
-        size,
-        disabled,
-      })}
+      type={type}
+      className={clsx(styles.iconButton({ size, disabled }), className)}
       disabled={disabled}
-      onClick={onClick}
+      {...props}
     >
-      <IconsResponsive size={BTN_ICON_SIZE[size]} name={name} />
+      <Icon size={ICON_BUTTON_SIZE[size]} name={name} />
     </button>
   );
 };
