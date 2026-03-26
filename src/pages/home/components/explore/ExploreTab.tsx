610d51af96f120c3ef9668dbd08b82937b8955e1
jstar_00 <wltjd6300@naver.com>
2026-03-26 15:38:00 +0900

    feat: 배너 라우팅 로직 추가
    - generatePath 기반 동적 라우팅 적용

--- src/pages/home/components/explore/ExploreTab.tsx
+++ src/pages/home/components/explore/ExploreTab.tsx
@@ -1,6 +1,10 @@
+import { generatePath, useNavigate } from 'react-router-dom';
+
 import Banner, {
   type BannerSlide,
 } from '@pages/home/components/explore/banner/Banner';
+
+import { ROUTES } from '@routes/paths';
 
 import imgBanner01 from '@assets/v2/images/ImgBanner_01.png';
 import imgBanner02 from '@assets/v2/images/ImgBanner_02.png';
@@ -19,8 +23,12 @@ const BANNER_SLIDES_MOCK: BannerSlide[] = [
 ];
 
 const ExploreTab = () => {
-  const handleBannerSlideClick = (_slide: BannerSlide) => {
-    // TODO: 슬라이드별 디테일 페이지 이동 등 연동
+  const navigate = useNavigate();
+
+  const handleBannerSlideClick = (slide: BannerSlide) => {
+    navigate(
+      generatePath(ROUTES.BANNER_DETAIL, { bannerId: String(slide.id) })
+    );
   };
 
   return (
