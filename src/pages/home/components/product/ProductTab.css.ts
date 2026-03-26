a74d4ff6192206431c46fb5254df43a19c75b00a
earl9rey <earl9rey@naver.com>
2026-03-26 15:18:31 +0900

    style: 상품 탭 전체 css 적용

--- src/pages/home/components/product/ProductTab.css.ts
+++ src/pages/home/components/product/ProductTab.css.ts
@@ -1,8 +1,14 @@
 import { style } from '@vanilla-extract/css';
 
+import { unitVars } from '@styles/tokensV2/unit.css';
+
 export const container = style({
+  boxSizing: 'border-box',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
+  alignItems: 'stretch',
+  alignSelf: 'stretch',
   width: '100%',
+  minWidth: unitVars.unit.dimension.wMin,
+  maxWidth: unitVars.unit.dimension.wMax,
 });
