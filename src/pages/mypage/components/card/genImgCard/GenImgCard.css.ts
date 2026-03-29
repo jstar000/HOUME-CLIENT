564ec5d991aecabc590b444574c52b1ccd846024
Soy <da5868@naver.com>
2026-03-29 16:31:16 +0900

    feat: GenImgCard 컴포넌트 구현 및 테스트 컴포넌트

---
+++ src/pages/mypage/components/card/genImgCard/GenImgCard.css.ts
@@ -0,0 +1,64 @@
+import { style } from '@vanilla-extract/css';
+
+import { colorVars } from '@/shared/styles/tokensV2/color.css';
+import { fontVars } from '@/shared/styles/tokensV2/font.css';
+import { unitVars } from '@/shared/styles/tokensV2/unit.css';
+
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: unitVars.unit.gapPadding['200'],
+  padding: unitVars.unit.gapPadding['000'],
+  width: '100%',
+});
+
+export const textContainer = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+export const headingTexxt = style({
+  display: 'flex',
+  ...fontVars.font.title_sb_15,
+  padding: `${unitVars.unit.gapPadding['000']} ${unitVars.unit.gapPadding['100']}`,
+  color: colorVars.color.text.primary,
+});
+
+export const imgContainer = style({
+  aspectRatio: '166 / 111',
+  display: 'flex',
+  flexShrink: 0,
+  alignItems: 'center',
+  transition: 'transform 100ms ease',
+  border: 0,
+  borderRadius: unitVars.unit.radius['300'],
+  width: '100%',
+  height: '22.4rem',
+  overflow: 'hidden',
+});
+
+export const cardImg = style({
+  objectFit: 'cover',
+  width: '100%',
+  height: '100%',
+});
+
+export const listCardContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  gap: unitVars.unit.gapPadding['200'],
+  padding: `${unitVars.unit.gapPadding['000']} ${unitVars.unit.gapPadding['100']}`,
+  width: '100%',
+
+  overflowX: 'auto',
+  scrollbarWidth: 'none',
+  whiteSpace: 'nowrap',
+  msOverflowStyle: 'none',
+
+  selectors: {
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+  },
+});
---
+++ src/pages/mypage/components/card/genImgCard/GenImgCard.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+
+import TestImage from '@assets/v2/images/testImg.png';
+import ArrowRight from '@assets/v2/svg/ArrowRight.svg?react';
+
+import TextButton from '@/shared/components/v2/btnText/TextButton';
+import ListCardProduct from '@/shared/components/v2/cardProduct/ListCardProduct';
+
+import * as styles from './GenImgCard.css';
+
+const listCardMockData = [
+  {
+    id: 1,
+    title: '리스트 상품명은 최대 한 줄까지 쓸 수 있어요.',
+    linkHref: 'https://example.com',
+    discountRate: 10,
+    discountPrice: 1000000,
+  },
+  {
+    id: 2,
+    title: '리스트 상품명은 최대 한 줄까지 쓸 수 있어요.',
+    linkHref: 'https://example.com',
+    discountRate: 0,
+    discountPrice: 500000,
+  },
+  {
+    id: 3,
+    title: '리스트 상품명은 최대 한 줄까지 쓸 수 있어요.',
+    linkHref: 'https://example.com',
+    discountPrice: 800000,
+  },
+  {
+    id: 4,
+    title: '리스트 상품명은 최대 한 줄까지 쓸 수 있어요.',
+    linkHref: 'https://example.com',
+    discountRate: 20,
+    discountPrice: 300000,
+  },
+];
+
+interface GenImgCardProps {}
+
+const GenImgCard = () => {
+  const [isSaved1, setIsSaved1] = useState(false);
+  return (
+    <div className={styles.wrapper}>
+      <section className={styles.textContainer}>
+        <span className={styles.headingTexxt}>
+          침대 외 4개의 가구로 생성된 이미지
+        </span>
+        <TextButton
+          color="secondary"
+          size="s"
+          rightIcon={<ArrowRight aria-hidden />}
+          onClick={() => {}}
+        >
+          더보기
+        </TextButton>
+      </section>
+      <section className={styles.imgContainer}>
+        <img src={TestImage} alt="" className={styles.cardImg} />
+      </section>
+      <section className={styles.listCardContainer}>
+        {listCardMockData.map((item) => (
+          <ListCardProduct
+            key={item.id}
+            cardSize="s"
+            title={item.title}
+            isSaved={isSaved1}
+            onToggleSave={() => setIsSaved1((prev) => !prev)}
+            linkHref={item.linkHref}
+            discountRate={item.discountRate}
+            discountPrice={item.discountPrice}
+          />
+        ))}
+      </section>
+    </div>
+  );
+};
+export default GenImgCard;
