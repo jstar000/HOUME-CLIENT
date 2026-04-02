890158368b19c157b3b708af14182654afc910a0
earl9rey <earl9rey@naver.com>
2026-04-02 13:00:58 +0900

    feat: 상품 카드 목업 데이터로 변경

--- src/pages/home/components/product/SearchSection/SearchSection.tsx
+++ src/pages/home/components/product/SearchSection/SearchSection.tsx
@@ -12,6 +12,61 @@ const SearchSection = () => {
   const [isPriceRangeSelected, setPriceRangeSelected] = useState(false);
   const [isColorSelected, setColorSelected] = useState(false);
 
+  const mockProducts = [
+    {
+      id: 'p1',
+      title: '모듈 패브릭 소파 3인',
+      discountRate: 25,
+      discountPrice: 399000,
+    },
+    {
+      id: 'p2',
+      title: '원목 라운드 테이블 1200',
+      discountRate: 15,
+      discountPrice: 289000,
+    },
+    {
+      id: 'p3',
+      title: '메탈 프레임 책상 1400',
+      discountRate: 30,
+      discountPrice: 179000,
+    },
+    {
+      id: 'p4',
+      title: '패브릭 암체어',
+      discountRate: 10,
+      discountPrice: 129000,
+    },
+    {
+      id: 'p5',
+      title: '수납형 침대 프레임 Q',
+      discountRate: 20,
+      discountPrice: 499000,
+    },
+    {
+      id: 'p6',
+      title: '심플 거실장 1800',
+      discountRate: 35,
+      discountPrice: 159000,
+    },
+    { id: 'p7', title: '스탠드 조명', discountRate: 5, discountPrice: 89000 },
+    { id: 'p8', title: '러그 150x200', discountRate: 40, discountPrice: 59000 },
+    { id: 'p9', title: '벽선반 2단', discountRate: 18, discountPrice: 39000 },
+    { id: 'p10', title: '데스크 체어', discountRate: 22, discountPrice: 99000 },
+    {
+      id: 'p11',
+      title: '침구 세트 (SS)',
+      discountRate: 12,
+      discountPrice: 79000,
+    },
+    {
+      id: 'p12',
+      title: '수납 바스켓 세트',
+      discountRate: 28,
+      discountPrice: 49000,
+    },
+  ];
+
   const handleFurnitureTypeChipClick = useCallback(() => {
     setFurnitureTypeSelected((prev) => !prev);
   }, []);
@@ -56,79 +111,17 @@ const SearchSection = () => {
       </div>
       <div className={styles.productList}>
         {/* TODO: V2 공컴으로 변경 */}
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
-        <CardProduct
-          size="large"
-          title="title"
-          isSaved={false}
-          onToggleSave={() => {}}
-        />
+        {mockProducts.map(({ id, title, discountRate, discountPrice }) => (
+          <CardProduct
+            key={id}
+            size="large"
+            title={title}
+            discountRate={discountRate}
+            discountPrice={discountPrice}
+            isSaved={false}
+            onToggleSave={() => {}}
+          />
+        ))}
       </div>
     </section>
   );
