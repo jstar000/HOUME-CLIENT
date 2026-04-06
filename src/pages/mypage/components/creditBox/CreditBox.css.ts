a407e6830c9447389221da9e5cf613616a92442a
Soy <da5868@naver.com>
2026-04-06 18:35:04 +0900

    feat: creditBox 컴포넌트

--- src/pages/mypage/components/creditBox/CreditBox.css.ts
+++ src/pages/mypage/components/creditBox/CreditBox.css.ts
@@ -5,16 +5,22 @@ import { fontVars } from '@/shared/styles/tokensV2/font.css';
 import { unitVars } from '@/shared/styles/tokensV2/unit.css';
 
 export const container = style({
-  display: 'inline-flex',
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexShrink: 0,
   alignItems: 'center',
+  gap: unitVars.unit.gapPadding['100'],
   borderRadius: unitVars.unit.radius.full,
   backgroundColor: colorVars.color.fill.weak,
-  padding: `${unitVars.unit.gapPadding['000']} ${unitVars.unit.gapPadding['200']}`,
+  padding: `${unitVars.unit.gapPadding['000']} ${unitVars.unit.gapPadding['300']} ${unitVars.unit.gapPadding['000']} ${unitVars.unit.gapPadding['200']}`,
   height: '3.6rem',
 });
 
 export const textWrapper = style({
-  padding: unitVars.unit.gapPadding['100'],
+  display: 'flex',
+  gap: '0.1rem',
+  width: '100%',
+  minWidth: '2.7rem',
 });
 
 export const text = styleVariants({
