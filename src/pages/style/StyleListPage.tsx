45c3868c2d19002a40512d75039dc624ab510cb1
Soy <da5868@naver.com>
2026-04-08 16:24:11 +0900

    feat: 스타일 목록 페이지

--- src/pages/style/StyleListPage.tsx
+++ src/pages/style/StyleListPage.tsx
@@ -1,5 +1,35 @@
+import { generatePath, useNavigate } from 'react-router-dom';
+
+import { ROUTES } from '@/routes/paths';
+import TitleNavBar from '@/shared/components/navBar/TitleNavBar';
+import StyleCard from '@/shared/components/v2/styleCard/StyleCard';
+
+import { STYLELIST_MOCK } from './mocks/styleDetail';
+import * as styles from './StyleListPage.css';
+
 const StyleListPage = () => {
-  return <div>스타일 목록 페이지</div>;
+  const navigate = useNavigate();
+  const handleStyleClick = (styleId: number) => {
+    navigate(generatePath(ROUTES.STYLE_DETAIL, { styleId: String(styleId) }));
+  };
+
+  return (
+    <section className={styles.wrapper}>
+      <TitleNavBar title="스타일 전체 보기" />
+      <div className={styles.cardList}>
+        {/* TODO: 컴포넌트 수정 반영 필요 */}
+        {STYLELIST_MOCK.data.otherStyles.map((style) => (
+          <StyleCard
+            key={style.id}
+            imageSrc={style.imageUrl}
+            title={style.name}
+            onClick={() => handleStyleClick(style.id)}
+            imageLoading="eager"
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default StyleListPage;
