8970830a2926c2a6f489c1fda2ae99ded4e50b92
earl9rey <earl9rey@naver.com>
2026-04-09 16:39:46 +0900

    style: alignItems 오류값 수정

--- src/pages/banner/BannerDetailPage.css.ts
+++ src/pages/banner/BannerDetailPage.css.ts
@@ -29,7 +29,7 @@ export const bannerContainer = style({
 export const questionContainer = style({
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'left',
+  alignItems: 'flex-start',
   gap: unitVars.unit.gapPadding['400'],
   width: '100%',
 });
