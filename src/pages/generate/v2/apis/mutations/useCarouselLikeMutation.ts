6b9590c52791b68616ea94599ecfa3afa0f725e5
Soy <da5868@naver.com>
2026-04-24 05:22:30 +0900

    fix: 좋아요 성공시 찜 쿼리 무효화

--- src/pages/generate/v2/apis/mutations/useCarouselLikeMutation.ts
+++ src/pages/generate/v2/apis/mutations/useCarouselLikeMutation.ts
@@ -1,8 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 
+import { queryClient } from '@apis/config/queryClient';
 import { HTTPMethod, request } from '@apis/config/request';
 
 import { API_ENDPOINT } from '@constants/apiEndpoints';
+import { queryKeys } from '@constants/queryKey';
 
 export const postCarouselLike = async (rawProductId: number): Promise<void> => {
   return request({
@@ -15,5 +17,8 @@ export const postCarouselLike = async (rawProductId: number): Promise<void> => {
 export const usePostCarouselLikeMutation = () => {
   return useMutation({
     mutationFn: postCarouselLike,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queryKeys.jjym.list() });
+    },
   });
 };
