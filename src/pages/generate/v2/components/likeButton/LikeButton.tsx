b52bbf609c7a851c0f11841c7bba3e05373823cf
Soy <da5868@naver.com>
2026-04-24 00:23:27 +0900

    feat: loading like, dislike 버튼 컴포넌트

---
+++ src/pages/generate/v2/components/likeButton/LikeButton.tsx
@@ -0,0 +1,34 @@
+// like, dislike 버튼
+import Icon from '@components/v2/icon/Icon';
+
+import * as styles from './LikeButton.css';
+
+export interface LikeButtonProps
+  extends Omit<React.ComponentProps<'button'>, 'children'> {
+  name: 'like' | 'dislike';
+}
+
+const LikeButton = ({
+  name,
+  type = 'button',
+  disabled = false,
+  ...props
+}: LikeButtonProps) => {
+  const isLike = name === 'like';
+  return (
+    <button
+      type={type}
+      className={styles.likeButton({ name, disabled })}
+      disabled={disabled}
+      {...props}
+    >
+      {isLike ? (
+        <Icon name={'HeartFillWhite'} size={'32'} />
+      ) : (
+        <Icon name={'DislikeWhite'} size={'32'} />
+      )}
+    </button>
+  );
+};
+
+export default LikeButton;
