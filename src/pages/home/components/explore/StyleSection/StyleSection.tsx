bf2f159419225d75e320f99b0a93c3330ed5ae23
Soy <da5868@naver.com>
2026-04-24 16:51:29 +0900

    feat: 스타일 전체 조회 api 연동

--- src/pages/home/components/explore/StyleSection/StyleSection.tsx
+++ src/pages/home/components/explore/StyleSection/StyleSection.tsx
@@ -2,45 +2,36 @@ import { generatePath, useNavigate } from 'react-router-dom';
 
 import { ROUTES } from '@routes/paths';
 
+import InlineError from '@components/inlineError/InlineError';
+import Loading from '@components/loading/Loading';
+import TextButton from '@components/v2/btnText/TextButton';
 import StyleCard from '@components/v2/styleCard/StyleCard';
 
-import TextButton from '@/shared/components/v2/btnText/TextButton';
+import { useGetStyleListQuery } from '@/shared/apis/queries/useGetStyleQuery';
 
 import * as styles from './StyleSection.css';
-
-const STYLE_MOCK = [
-  {
-    id: 'minimal',
-    imageSrc: ' ',
-    title: '미니멀한 개발자의 집',
-  },
-  {
-    id: 'kitsch',
-    imageSrc: ' ',
-    title: '키치한 무드의 집',
-  },
-  {
-    id: 'animal',
-    imageSrc: ' ',
-    title: '반려동물과 함께 하는 집',
-  },
-  {
-    id: 'blue',
-    imageSrc: ' ',
-    title: '블루 아이템이 포인트인 집',
-  },
-] as const;
 
 const StyleSection = () => {
   const navigate = useNavigate();
 
-  const handleStyleClick = (styleId: string) => {
-    navigate(generatePath(ROUTES.STYLE_DETAIL, { styleId }));
+  const handleStyleClick = (styleId: number) => {
+    navigate(
+      generatePath(ROUTES.STYLE_DETAIL, { styleId: styleId.toString() })
+    );
   };
 
   const handleMoreClick = () => {
     navigate(ROUTES.STYLE_LIST);
   };
+
+  const {
+    data: stylesData,
+    isPending,
+    isError,
+    refetch,
+  } = useGetStyleListQuery(4);
+
+  console.log('stylesData:', stylesData);
 
   return (
     <section className={styles.section}>
@@ -55,16 +46,32 @@ const StyleSection = () => {
           더보기
         </TextButton>
       </div>
+
       <div className={styles.cardGrid}>
-        {STYLE_MOCK.map((style) => (
-          <StyleCard
-            key={style.id}
-            imageSrc={style.imageSrc}
-            title={style.title}
-            onClick={() => handleStyleClick(style.id)}
-            imageLoading="eager"
+        {isError ? (
+          <InlineError
+            onRetry={refetch}
+            message="다른 스타일을 불러올 수 없습니다"
           />
-        ))}
+        ) : isPending ? (
+          <Loading />
+        ) : (
+          <>
+            {stylesData.map((style) => (
+              <StyleCard
+                key={style.id}
+                imageSrc={style.imageUrl || ''}
+                title={style.name}
+                onClick={() => {
+                  if (typeof style.id === 'number') {
+                    handleStyleClick(style.id);
+                  }
+                }}
+                imageLoading="eager"
+              />
+            ))}
+          </>
+        )}
       </div>
     </section>
   );
