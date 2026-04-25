61f69154274c67a999238f505af8c743fc464cda
earl9rey <earl9rey@naver.com>
2026-04-25 15:00:29 +0900

    feat: 탐색 탭 배너 API 1차 연동

--- src/pages/home/components/explore/ExploreTab.tsx
+++ src/pages/home/components/explore/ExploreTab.tsx
@@ -6,21 +6,9 @@ import Banner, {
 
 import { ROUTES } from '@routes/paths';
 
-import imgBanner01 from '@assets/v2/images/ImgBanner_01.png';
-import imgBanner02 from '@assets/v2/images/ImgBanner_02.png';
-import imgBanner03 from '@assets/v2/images/ImgBanner_03.png';
-import imgBanner04 from '@assets/v2/images/ImgBanner_04.png';
-
 import * as styles from './ExploreTab.css';
 import RoomTypeSection from './RoomTypeSection/RoomTypeSection';
 import StyleSection from './StyleSection/StyleSection';
-
-const BANNER_SLIDES_MOCK: BannerSlide[] = [
-  { id: 1, title: '잦은 재택근무에 딱 맞는', imageUrl: imgBanner01 },
-  { id: 2, title: '창가에서 브런치 즐기는', imageUrl: imgBanner02 },
-  { id: 3, title: '친구 초대하기 좋은', imageUrl: imgBanner03 },
-  { id: 4, title: 'OTT 감상하기 좋은', imageUrl: imgBanner04 },
-];
 
 const ExploreTab = () => {
   const navigate = useNavigate();
@@ -34,10 +22,7 @@ const ExploreTab = () => {
 
   return (
     <div className={styles.container}>
-      <Banner
-        slides={BANNER_SLIDES_MOCK}
-        onSlideClick={handleBannerSlideClick}
-      />
+      <Banner onSlideClick={handleBannerSlideClick} />
       <div className={styles.content}>
         <RoomTypeSection />
         <StyleSection />
