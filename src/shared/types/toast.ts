e7ecfabdcd8cf759ab63b610906ed75a7b67b4ac
Soy <da5868@naver.com>
2026-05-21 18:56:51 +0900

    type: toast type 수정

--- src/shared/types/toast.ts
+++ src/shared/types/toast.ts
@@ -1,19 +1,35 @@
 import { Slide } from 'react-toastify';
 
 import type { ToastContainerProps } from 'react-toastify';
+import type { ToasterProps } from 'sonner';
 
 export const TOAST_TYPE = {
   INFO: 'info',
-  WARNING: 'warning',
-  NAVIGATE: 'navigate',
+  SUCCESS: 'success',
+
+  ERROR: 'error',
+  ACTION: 'action',
 } as const;
 
-// 1) 키 타입: 'SUCCESS' | 'WARNING'
+// 1) 키 타입
 export type ToastTypeKey = keyof typeof TOAST_TYPE;
 
-// 2) 값 타입: 'success' | 'warning'
+// 2) 값 타입
 export type ToastType = (typeof TOAST_TYPE)[ToastTypeKey];
 
+// sonner의 Toaster 기본 설정
+export const TOASTER_DEFAULTS = {
+  position: 'bottom-center',
+  visibleToasts: 1,
+  closeButton: false,
+  duration: 2000,
+  expand: false, // hover시 확장
+} satisfies Pick<
+  ToasterProps,
+  'position' | 'visibleToasts' | 'closeButton' | 'duration' | 'expand'
+>;
+
+// react-toastify의 ToastContainer 기본 설정 (일단 삭제 안함)
 export const toastStyle = {
   backgroundColor: 'transparent',
   boxShadow: 'none',
