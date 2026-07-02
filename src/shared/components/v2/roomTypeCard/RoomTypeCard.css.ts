2077e267b0f0750308cd8b28b670b6431367d799
earl9rey <earl9rey@naver.com>
2026-07-02 18:55:40 +0900

    feat: RoomTypeCard press 인터랙션 토큰 적용

--- src/shared/components/v2/roomTypeCard/RoomTypeCard.css.ts
+++ src/shared/components/v2/roomTypeCard/RoomTypeCard.css.ts
@@ -3,6 +3,7 @@ import { recipe } from '@vanilla-extract/recipes';
 
 import { colorVars } from '@styles/tokensV2/color.css';
 import { fontVars } from '@styles/tokensV2/font.css';
+import { transition } from '@styles/tokensV2/interaction/interaction.utils';
 import { unitVars } from '@styles/tokensV2/unit.css';
 
 export const optionCard = recipe({
@@ -11,14 +12,14 @@ export const optionCard = recipe({
     display: 'flex',
     flexShrink: 0,
     alignItems: 'flex-start',
-    transition: 'transform 100ms ease',
+    transition: transition('transform', 'fastest', 'bezier.out'),
     border: 0,
     borderRadius: unitVars.unit.radius['600'],
     overflow: 'hidden',
     textAlign: 'left',
     selectors: {
       '&:active': {
-        transform: 'scale(0.98)',
+        transform: 'scale(0.95)',
       },
     },
   },
