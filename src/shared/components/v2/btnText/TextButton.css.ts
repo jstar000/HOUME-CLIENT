fbee16cda1383a878f817235b777cdc9dcb4eb10
earl9rey <earl9rey@naver.com>
2026-07-02 19:58:15 +0900

    feat: TextButton press 인터랙션 토큰 적용

--- src/shared/components/v2/btnText/TextButton.css.ts
+++ src/shared/components/v2/btnText/TextButton.css.ts
@@ -3,6 +3,7 @@ import { recipe } from '@vanilla-extract/recipes';
 
 import { colorVars } from '@styles/tokensV2/color.css';
 import { fontVars } from '@styles/tokensV2/font.css';
+import { transition } from '@styles/tokensV2/interaction/interaction.utils';
 import { unitVars } from '@styles/tokensV2/unit.css';
 
 export const button = recipe({
@@ -11,10 +12,11 @@ export const button = recipe({
     alignItems: 'center',
     justifyContent: 'center',
     gap: unitVars.unit.gapPadding['000'],
-    transition: 'transform 100ms ease',
+    transformOrigin: 'center center',
+    transition: transition('transform', 'fastest', 'bezier.out'),
 
     selectors: {
-      '&:active': {
+      '&:not(:disabled):active': {
         transform: 'scale(0.95)',
       },
     },
