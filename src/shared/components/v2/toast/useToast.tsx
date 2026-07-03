90917293b5605b1c7a92dba38820b205d38bd720
earl9rey <earl9rey@naver.com>
2026-07-03 15:06:58 +0900

    fix: 스낵바 duration: infinity 제거

--- src/shared/components/v2/toast/useToast.tsx
+++ src/shared/components/v2/toast/useToast.tsx
@@ -51,7 +51,6 @@ export const useToast = () => {
             />
           ),
           {
-            duration: Infinity,
             ...options,
             style: {
               ...toastStyle,
